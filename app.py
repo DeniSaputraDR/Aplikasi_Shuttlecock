a6b5f7b8856d974d00afc339e8a3e8bc7befc14f
Deni <153747604+DeniSaputraDR@users.noreply.github.com>
2025-08-24 15:44:37 +0700

    thir2

--- app.py
+++ app.py
@@ -109,6 +109,6 @@ def capture():
     )
 
 
-if __name__ == "_main_":
-    port = int(os.environ.get("PORT", 7860))  
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port)
