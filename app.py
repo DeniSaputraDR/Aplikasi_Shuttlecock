908039ad1d680d95f382600ef76ac0a403eaf8ab
Deni <153747604+DeniSaputraDR@users.noreply.github.com>
2025-08-24 15:21:16 +0700

    frist

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+import numpy as np
+from keras.models import load_model
+from PIL import Image
+import base64
+import io
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['WEBCAM_FOLDER'] = 'static/webcam_capture'
+
+model = load_model('model/model_shuttlecock.h5')
+class_names = ['Layak', 'Tidak Layak']
+
+def predict_image(image_path):
+    img = Image.open(image_path).convert('RGB')
+    img = img.resize((224, 224))
+    x = np.array(img) / 255.0
+    x = np.expand_dims(x, axis=0)
+    prediction = model.predict(x)
+
+    if prediction[0][0] > 0.5:
+        label = 'Tidak Layak'
+        confidence = prediction[0][0] * 100
+        saran = "Shuttlecock sebaiknya diganti, tidak layak untuk permainan serius."
+        visual = "Bulu rusak, bengkok atau tidak merata."
+    else:
+        label = 'Layak'
+        confidence = (1 - prediction[0][0]) * 100
+        saran = "Shuttlecock masih dapat digunakan untuk latihan atau pertandingan."
+        visual = "Bulu terlihat rapi & utuh."
+
+    return label, confidence, saran, visual
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' in request.files:
+        file = request.files['file']
+        if file.filename != '':
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+
+            label, confidence, saran, visual = predict_image(filepath)
+
+            return render_template('index.html', 
+                                   image_url=filepath,
+                                   label=label,
+                                   confidence=confidence,
+                                   saran=saran,
+                                   visual=visual)
+    return render_template('index.html', error="Upload gagal.")
+
+@app.route('/capture', methods=['POST'])
+def capture():
+    data_url = request.form['image_data']
+    header, encoded = data_url.split(',', 1)
+    img_data = base64.b64decode(encoded)
+    image = Image.open(io.BytesIO(img_data))
+    filename = 'capture.jpg'
+    filepath = os.path.join(app.config['WEBCAM_FOLDER'], filename)
+    image.save(filepath)
+
+    label, confidence, saran, visual = predict_image(filepath)
+
+    return render_template('index.html', 
+                           image_url=filepath,
+                           label=label,
+                           confidence=confidence,
+                           saran=saran,
+                           visual=visual)
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))  # penting: HuggingFace pakai $PORT
+    app.run(host="0.0.0.0", port=port)
